4d1289a2e42803bd30de78cd83e6af977051198f
IvanPratasevich <iv.pratasevich@gmail.com>
2022-06-03 17:37:38 +0300

    feat: add solution for copy.js

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,3 +1,58 @@
-export const copy = async () => {
-    // Write your code here 
+import { readdir, mkdir, copyFile } from 'node:fs/promises';
+import { dirname, join } from 'node:path';
+import { fileURLToPath } from 'node:url';
+import * as fs from 'node:fs';
+
+const copyDirectory = async (src, destination) => {
+  const items = await readdir(src, { withFileTypes: true });
+  await mkdir(destination);
+  for (const item of items) {
+    const srcPath = join(src, item.name);
+    const destinationPath = join(destination, item.name);
+    if (item.isDirectory()) {
+      await copyDirectory(srcPath, destinationPath);
+    } else {
+      await copyFile(srcPath, destinationPath);
+    }
+  }
 };
+
+export const copy = async () => {
+  const __dirname = await getDirname(import.meta.url);
+  const pathToSourceDir = join(__dirname, 'files');
+  const pathToCopyDir = join(__dirname, 'files_copy');
+  try {
+    const isSourceDirExists = await exists(pathToSourceDir);
+    const isCopyDirExists = await exists(pathToCopyDir);
+    if (!isSourceDirExists || isCopyDirExists) {
+      throw new Error('FS operation failed');
+    }
+    await copyDirectory(pathToSourceDir, pathToCopyDir);
+  } catch (error) {
+    process.stderr.write(error.message);
+    process.exit(1);
+  }
+};
+
+const exists = (path) => {
+  return new Promise((resolve) => {
+    fs.access(path, fs.constants.F_OK, (error) => {
+      if (!error) {
+        resolve(true);
+      } else {
+        resolve(false);
+      }
+    });
+  });
+};
+
+const getDirname = async (url) => {
+  return new Promise((resolve) => {
+    const __filename = fileURLToPath(url);
+    const __dirname = dirname(__filename);
+    resolve(__dirname);
+  });
+};
+
+copy();
+
