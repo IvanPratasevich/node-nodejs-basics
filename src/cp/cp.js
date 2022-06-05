a94a67fe9964141b1707cc75d4e4441b34302ddf
IvanPratasevich <iv.pratasevich@gmail.com>
2022-06-05 22:33:06 +0300

    refactor: refactor code of task cp.js

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -3,16 +3,14 @@ import { join } from 'node:path';
 import { exists, getDirname } from '../additions/additions.js';
 
 export const spawnChildProcess = async (args) => {
-  args = args.map((el) => el.replace('--', ''));
   const __dirname = await getDirname(import.meta.url);
   const pathToFile = join(__dirname, 'files', 'script.js');
   const isFileExists = await exists(pathToFile);
   if (!isFileExists) {
     throw new Error('FS operation failed');
   }
-  const child = fork(pathToFile, [...args], { silent: true });
-  process.stdin.pipe(child.stdin);
-  child.stdout.pipe(process.stdout);
+  fork(pathToFile, args);
 };
 
 spawnChildProcess(process.argv.splice(2));
+
